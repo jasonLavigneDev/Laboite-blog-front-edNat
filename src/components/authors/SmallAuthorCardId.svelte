c829182733992d44986677d025f7de86988e8ed5
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2021-01-21 16:15:17 +0100

    remove username from authors, fix diplay if structure is not defined

--- src/components/authors/SmallAuthorCardId.svelte
+++ src/components/authors/SmallAuthorCardId.svelte
@@ -1,20 +1,13 @@
 <script>
-  import { _ } from "svelte-i18n";
-  import { structureOptions } from "../../routes/academies/_academies";
-  import BigLink from "../common/BigLink.svelte";
-  import FavoritesButton from "../common/FavoritesButton.svelte";
-  import Avatar from "./Avatar.svelte";
+  import { _ } from 'svelte-i18n';
+  import { structureOptions } from '../../routes/academies/_academies';
+  import BigLink from '../common/BigLink.svelte';
+  import FavoritesButton from '../common/FavoritesButton.svelte';
+  import Avatar from './Avatar.svelte';
 
   export let author;
-  const academy =
-    structureOptions.find(({ value }) => value === author.structure) || {};
+  const academy = structureOptions.find(({ value }) => value === (author.structure || 'Autre')) || {};
 </script>
-
-<style>
-  .title {
-    margin-bottom: 0px;
-  }
-</style>
 
 <div class="column is-full ">
   <div class="media box">
@@ -30,10 +23,14 @@
       </div>
     </div>
     <div class="media-right">
-      <BigLink
-        link="/authors/{author._id}"
-        text={$_('components.SmallAuthorIdCard.all_articles')} />
+      <BigLink link="/authors/{author._id}" text={$_('components.SmallAuthorIdCard.all_articles')} />
       <FavoritesButton type="author" itemId={author._id} />
     </div>
   </div>
 </div>
+
+<style>
+  .title {
+    margin-bottom: 0px;
+  }
+</style>
--- src/routes/academies/[slug]/authors/index.svelte
+++ src/routes/academies/[slug]/authors/index.svelte
@@ -1,9 +1,9 @@
 <script context="module">
-  import { fetchData } from "../../../../utils/api/methods";
-  import { structureOptions } from "../../_academies";
+  import { fetchData } from '../../../../utils/api/methods';
+  import { structureOptions } from '../../_academies';
 
   export async function preload({ params, query, path }, { env }) {
-    const { page = 1, search = "" } = query;
+    const { page = 1, search = '' } = query;
     const academy = structureOptions.find(({ slug }) => slug === params.slug);
     const isResearchLink = !!search;
     const request = !!search
@@ -11,15 +11,15 @@
           path,
           query: { search },
           academy,
-          type: "authors",
+          type: 'authors',
         }
       : null;
 
     const limit = 9;
     const fields = {};
-    const searchFields = ["firstName", "lastName", "username", "structure"];
-    const order = "articlesCount DESC";
-    const apiurl = "authors";
+    const searchFields = ['firstName', 'lastName', 'structure'];
+    const order = 'articlesCount DESC';
+    const apiurl = 'authors';
     const where = { articlesCount: { gt: 0 }, structure: academy.value };
 
     const { items, total } = await fetchData({
@@ -48,23 +48,23 @@
 </script>
 
 <script>
-  import { _ } from "svelte-i18n";
-  import { stores } from "@sapper/app";
-  import Divider from "../../../../components/common/Divider.svelte";
-  import SearchField from "../../../../components/common/SearchField.svelte";
-  import Pagination from "../../../../components/common/Pagination.svelte";
-  import NoResults from "../../../../components/common/NoResults.svelte";
-  import AuthorIdCard from "../../../../components/authors/AuthorIdCard.svelte";
-  import BackButton from "../../../../components/navigation/BackButton.svelte";
-  import PageTransition from "../../../../components/common/PageTransition.svelte";
-  import FavoritesButton from "../../../../components/common/FavoritesButton.svelte";
+  import { _ } from 'svelte-i18n';
+  import { stores } from '@sapper/app';
+  import Divider from '../../../../components/common/Divider.svelte';
+  import SearchField from '../../../../components/common/SearchField.svelte';
+  import Pagination from '../../../../components/common/Pagination.svelte';
+  import NoResults from '../../../../components/common/NoResults.svelte';
+  import AuthorIdCard from '../../../../components/authors/AuthorIdCard.svelte';
+  import BackButton from '../../../../components/navigation/BackButton.svelte';
+  import PageTransition from '../../../../components/common/PageTransition.svelte';
+  import FavoritesButton from '../../../../components/common/FavoritesButton.svelte';
 
   export let authors = [];
   export let total = 0;
   export let limit;
   export let page = 1;
   export let query = {};
-  export let path = "";
+  export let path = '';
   export let academy;
   export let isResearchLink;
   export let request;
@@ -78,16 +78,15 @@
 <PageTransition>
   <BackButton previousLocation="/academies/{academy.slug}" />
   <div class="container box-transparent">
-    <a href="/academies/{academy.slug}" rel="prefetch"><h1 class="title is-2">
+    <a href="/academies/{academy.slug}" rel="prefetch"
+      ><h1 class="title is-2">
         {academy.label}
-      </h1></a>
+      </h1></a
+    >
   </div>
   <section class="box-transparent">
     <div class="columns is-multiline is-mobile">
-      <div
-        class="column"
-        class:is-full={!isResearchLink}
-        class:is-half={isResearchLink}>
+      <div class="column" class:is-full={!isResearchLink} class:is-half={isResearchLink}>
         <h1 class="title">{$_('pages.authors.title')}: {total}</h1>
         <h2 class="subtitle">{$_('pages.authors.subtitle')}</h2>
       </div>
