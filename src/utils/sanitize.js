1e5f84f4d7ddba37a891c2cee3c72bf2bb69d515
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2023-09-26 11:17:45 +0200

    fix(articles): adjust sanitizeHtml parameters for articles

--- src/utils/sanitize.js
+++ src/utils/sanitize.js
@@ -1,16 +1,33 @@
 import sanitizeHtml from 'sanitize-html';
 
 // allow iframes for embedded videos in blog articles
-const sanitizeParameters = {
-  allowedTags: sanitizeHtml.defaults.allowedTags.concat(['iframe']),
+export const sanitizeParameters = {
+  allowedTags: sanitizeHtml.defaults.allowedTags.concat([
+    'iframe',
+    'img',
+    'audio',
+    'video',
+  ]),
   allowedAttributes: {
-    ...sanitizeHtml.defaults.allowedClasses,
+    ...sanitizeHtml.defaults.allowedAttributes,
     iframe: ['src', 'frameborder', 'allowfullscreen'],
     span: ['contenteditable'],
+    audio: ['preload', 'controls', 'src'],
+    video: ['preload', 'controls', 'src', 'width'],
+    a: ['href', 'name', 'target', 'rel'],
   },
   allowedClasses: {
     ...sanitizeHtml.defaults.allowedClasses,
     iframe: ['ql-video'],
+    div: [
+      'embed-audio',
+      'audio-wrapper',
+      'embed-responsive',
+      'webcam-video-wrapper',
+    ],
+    audio: ['embed-responsive-audio-item'],
+    video: ['embed-responsive-item'],
+    p: ['ql-indent-*'],
   },
 };
 
